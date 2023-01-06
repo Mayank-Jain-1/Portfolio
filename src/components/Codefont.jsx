0cba926b102923b718d874fc1c35cca97d80ab91
Mayank <kaushalmayank.k@gmail.com>
2023-01-07 03:02:03 +0530

    Font spacing for codeFont increased

--- src/components/Codefont.jsx
+++ src/components/Codefont.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Codefont = ({text,className}) => {
   return (
-    <div className={`text-neutral-600 font-semibold italic font-mark ${className}`}>&lt;{text}&gt;</div>
+    <div className={`text-neutral-600 font-semibold italic font-mark tracking-[0.13em] :text-lg ${className}`}>&lt;{text}&gt;</div>
   )
 }
 
