1d76182ec16b417d6f041d2e26fbef2298622323
Mayank <kaushalmayank.k@gmail.com>
2023-01-14 01:47:47 +0530

    New logic for making the jumping text with images included

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -13,8 +13,8 @@ const Sidebar = () => {
         isOpen ? "opacity-100 z-10" : "opacity-0 -z-10"
       }  lg:z-10 lg:opacity-100 flex flex-col justify-between bg-darker w-full lg:w-36 h-screen duration-300`}
     >
-      <div className="bg-black pt-5 lg:pt-0">
-        <img src={logo} className="h-36 lg:h-52 object-cover m-auto lg:mt-0" alt="" />
+      <div className="bg-black pt-5 lg:pt-0 flex justify-center">
+        <img src={logo} className="h-40 lg:h-52 object-cover" alt="" />
       </div>
       <Navlinks />
       <Profilelinks />
--- src/pages/landing.jsx
+++ src/pages/landing.jsx
@@ -1,11 +1,11 @@
 import React from "react";
 import Codefont from "../components/Codefont";
 import CrashText from "../components/CrashText";
+import J from "../media/J4.png";
+import M from "../media/M.png";
 
 const landing = () => {
-  const mainText1 = "Hi,";
-  const mainText2 = "I'm    ayank    ain,";
-  const mainText3 = "web developer";
+  console.log(J);
   const convertToCrash = (str) => {
     var result = [];
     for (let i = 0; i < str.length; i++) {
@@ -13,34 +13,64 @@ const landing = () => {
     }
     return result;
   };
+  const mainText = [
+    "Hi,",
+    "<br/>",
+    "I'm ",
+    M,
+    "ayank ",
+    J,
+    "ain,",
+    "<br/>",
+    "web developer",
+  ];
+  var result = [];
+  for (let i = 0; i < mainText.length; i++) {
+    const element = mainText[i];
+    if (element === "<br/>") {
+      result.push(<br />);
+    } else if (element.includes("png")) {
+      result.push(
+        <div className="inline-block">
+          <img src={element} alt="" className=" h-7 sm:h-10 md:h-16 lg:h-20
+          mr-1" />
+        </div>
+      );
+    } else {
+      var temp = convertToCrash(element);
+      result.push(...temp);
+    }
+  }
+  const mainText1 = "Hi,";
+  const mainText2 = "I'm    ayank    ain,";
+  const mainText3 = "web developer";
   const crash1 = convertToCrash(mainText1);
   const crash2 = convertToCrash(mainText2);
-  console.log(crash2);
+  // console.log(crash2);
   const crash3 = convertToCrash(mainText3);
 
   return (
     <section
       className="
-     px-4 lg:px-40 flex flex-col h-screen justify-center relative"
+     pl-4 lg:pl-40 flex flex-col h-screen justify-center relative"
     >
-      <div className="absolute top-20">
+      <div className="absolute top-16 lg:top-5">
         <Codefont text="!html" />
         <Codefont text="body" className="ml-4" />
       </div>
 
       <div className="">
         <Codefont text="h1" className="ml-4" />
+
         <div className="ml-3 lg:ml-8">
-          {crash1}
-          <br />
-          {crash2}
-          <br />
-          {crash3}
+          {result}
           <Codefont text="/h1" className={"inline-block ml-5"} />
         </div>
 
         <Codefont text="p" className="ml-4" />
-        <p className="ml-4 lg:ml-10 text-neutral-500">Front End Devloper / React Developer</p>
+        <p className="ml-4 lg:ml-10 text-neutral-500">
+          Front End Devloper / React Developer
+        </p>
         <Codefont text="/p" className="ml-4" />
       </div>
     </section>
