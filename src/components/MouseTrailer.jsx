41849e9fcf073b679a2296fbdba2f3ebcc823489
Mayank <kaushalmayank.k@gmail.com>
2023-01-19 01:57:04 +0530

    CHanged remove logic for mouse trail

--- src/components/MouseTrailer.jsx
+++ src/components/MouseTrailer.jsx
@@ -1,10 +1,11 @@
-import React, { useEffect, useRef } from "react";
+import React, { useEffect, useRef, useState } from "react";
 
 //TODO
 //Move all the required functions to the ../function/trailerFunctions.js
 
 const MouseTrailer = () => {
   const canvas = useRef();
+  const [windowWidth , setWindowWidth] = useState(0)
   const target = {
     x: 0,
     y: 0,
@@ -21,7 +22,6 @@ const MouseTrailer = () => {
 
   function mouseMove(event) {
     if (event.touches) {
-      
     } else {
       target.x = event.clientX;
       target.y = event.clientY;
@@ -30,10 +30,10 @@ const MouseTrailer = () => {
     event.preventDefault();
   }
 
-
   const resize = (ctx) => {
     ctx.canvas.width = window.innerWidth;
     ctx.canvas.height = window.innerHeight;
+    setWindowWidth(window.innerWidth);
   };
 
   function Tendril(options) {
@@ -61,61 +61,61 @@ const MouseTrailer = () => {
           this.nodes.push(node);
         }
       },
-      
+
       update: function (ctx) {
         var spring = this.spring,
-        node = this.nodes[0];
+          node = this.nodes[0];
         node.vx += (target.x - node.x) * spring;
         node.vy += (target.y - node.y) * spring;
         for (var prev, i = 0, n = this.nodes.length; i < n; i++) {
           node = this.nodes[i];
-          
+
           if (i > 0) {
             prev = this.nodes[i - 1];
-            
+
             node.vx += (prev.x - node.x) * spring;
             node.vy += (prev.y - node.y) * spring;
             node.vx += prev.vx * settings.dampening;
             node.vy += prev.vy * settings.dampening;
           }
-          
+
           node.vx *= this.friction;
           node.vy *= this.friction;
           node.x += node.vx;
           node.y += node.vy;
-          
+
           spring *= settings.tension;
         }
       },
-      
+
       draw: function (ctx) {
         var x = this.nodes[0].x,
-        y = this.nodes[0].y,
-        a,
-        b;
-        
+          y = this.nodes[0].y,
+          a,
+          b;
+
         ctx.beginPath();
         ctx.moveTo(x, y);
-        
+
         for (var i = 1, n = this.nodes.length - 2; i < n; i++) {
           a = this.nodes[i];
           b = this.nodes[i + 1];
           x = (a.x + b.x) * 0.5;
           y = (a.y + b.y) * 0.5;
-          
+
           ctx.quadraticCurveTo(a.x, a.y, x, y);
         }
-        
+
         a = this.nodes[i];
         b = this.nodes[i + 1];
-        
+
         ctx.quadraticCurveTo(a.x, a.y, b.x, b.y);
         ctx.stroke();
         ctx.closePath();
       },
     };
   })();
-  
+
   function reset() {
     tendrils = [];
 
@@ -131,14 +131,12 @@ const MouseTrailer = () => {
   var color = Math.random();
 
   const loop = (ctx) => {
-    if(ctx.canvas.width < 500) return ;
     ctx.globalCompositeOperation = "source-over";
     ctx.fillStyle = "#1d1d1d";
     ctx.fillRect(0, 0, ctx.canvas.width, ctx.canvas.height);
     ctx.globalCompositeOperation = "lighter";
     ctx.strokeStyle = "hsla(171,98%,56%,0.25)";
     ctx.lineWidth = 1;
-
 
     if (color < 0.5) {
       ctx.strokeStyle = "hsl(284, 91%, 15%)";
@@ -153,34 +151,29 @@ const MouseTrailer = () => {
     }
   };
 
-
   useEffect(() => {
     const ctx = canvas.current.getContext("2d");
     reset();
     resize(ctx);
-    console.log(ctx.canvas.width);
-    window.addEventListener('mousemove',mouseMove)
-    window.addEventListener("resize", () => resize(ctx));
-    const animate = setInterval(() => {
-      loop(ctx);
-    }, 1000 / 120);
-    
-    return () => {
-      window.removeEventListener('mousemove',mouseMove)
-      window.removeEventListener("resize", resize);
-      clearInterval(animate);
-    };
+
+    if (windowWidth > 768) {
+      window.addEventListener("mousemove", mouseMove);
+      window.addEventListener("resize", () => resize(ctx));
+      const animate = setInterval(() => {
+        loop(ctx);
+      }, 1000 / 120);
+
+      return () => {
+        window.removeEventListener("mousemove", mouseMove);
+        window.removeEventListener("resize", resize);
+        clearInterval(animate);
+      };
+    }
 
     // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
+  }, [windowWidth]);
 
-  return (
-    <canvas
-      id="canvas"
-      ref={canvas}
-      className="fixed -z-10 "
-    />
-  );
+  return <canvas id="canvas" ref={canvas} className="fixed -z-10 " />;
 };
 
 export default MouseTrailer;
