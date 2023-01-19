30de7df188e2a4b3931925b4c37e6ef4224b60e8
Mayank <kaushalmayank.k@gmail.com>
2023-01-19 14:26:57 +0530

    commit to fix vercel deployment

--- src/App.js
+++ src/App.js
@@ -2,8 +2,8 @@ import React, { useEffect } from "react";
 import MouseTrailer from "./components/MouseTrailer";
 import Sidebar from "./components/Sidebar";
 import Topbar from "./components/Topbar";
-import AboutMe from "./Pages/AboutMe";
-import Landing from "./Pages/Landing";
+import AboutMe from "./Pages/AboutMe.jsx";
+import Landing from "./Pages/Landing.jsx";
 import {
   updateViewHeight,
   updateScroll,
