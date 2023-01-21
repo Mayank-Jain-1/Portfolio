cf202ca82daa1454182cb26f86923820eea33cf3
Mayank <kaushalmayank.k@gmail.com>
2023-01-22 02:21:38 +0530

    Side bar logo turned to a link

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -13,8 +13,11 @@ const Sidebar = () => {
         isOpen ? "opacity-100 z-20" : "opacity-0 -z-10"
       }  lg:z-20 lg:opacity-100 flex flex-col justify-between bg-darker w-full lg:w-36 h-screen duration-300`}
     >
-      <div className="bg-black pt-5 lg:pt-0 flex justify-center">
+      <div  className="bg-black pt-5 lg:pt-0 flex justify-center">
+        <a href="toChange">
+
         <img src={logo} className="h-40 lg:h-52 object-cover" alt="" />
+        </a>
       </div>
       <Navlinks />
       <Profilelinks />
