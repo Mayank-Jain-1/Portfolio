12ed7fa98a9f6d3166a9c4cf66f9e68a27093872
Mayank <kaushalmayank.k@gmail.com>
2023-01-28 00:45:39 +0530

    added the text box over the map

--- src/Pages/ContactMe.jsx
+++ src/Pages/ContactMe.jsx
@@ -11,7 +11,7 @@ const ContactMe = () => {
   const crashText = arrayToCrash(mainText, crashTextStyle);
   return (
     <section className="w-full lg:h-screen mt-16 px-1 sm:px-3 md:px-6 lg:pl-8 lg:pr-0 relative flex flex-col  lg:flex-row lg:items-center justify-center">
-      <div className="lg:w-1/2">
+      <div className="lg:w-1/2 lg:mr-5">
         <Codefont text="h2" className="tab-1" />
         <div className="tab-2">{crashText}</div>
         <Codefont text="/h2" className="tab-1" />
@@ -24,14 +24,30 @@ const ContactMe = () => {
         <Codefont text="/p" className="tab-1" />
       </div>
 
-      <div className="border-none lg:w-1/2 m-4 lg:m-0  h-[300px] sm:h-[400px] md:h-[500px] lg:h-full
-      overflow-hidden flex justify-center items-center">
+      <div className="border-none lg:w-1/2 m-4 lg:ml-5 h-[300px] sm:h-[400px] md:h-[500px] lg:h-full
+      overflow-hidden flex justify-center items-center relative">
+
+        <div className="absolute bg-black bg-opacity-90 p-5 m-5 lg:m-12 top-0 left-0 text-white font-franklin text-xs md:text-base ">
+            <p>
+                Mayank Jain, <br />
+                Somewhere in <br />
+                Ludhiana, Punjab, India
+                <br />
+                <br />
+                <span className="text-brandGreen">@</span>
+                :
+                <br />
+                mayankjainx404@gmail.com
+            </p>
+        </div>
+
         <iframe
           src="https://snazzymaps.com/embed/457201"
           width="100%"
           height="100%"
           className="hidden lg:block  max-h-[1200px] max-w-[1000px]"
         ></iframe>
+
         <img src={mapSm} width='1000px' height='100%' alt="" className="max-w-none object-cover lg:hidden" />
       </div>
     </section>
