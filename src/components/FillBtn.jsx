77dd785dd7bb53bb20574de5dfeb30faf38cb2a7
Mayank <kaushalmayank.k@gmail.com>
2023-01-28 16:19:39 +0530

    changed the fillbtn filling directiona and removed the anchor tag replaced with onCLick arguement funciton

--- src/components/FillBtn.jsx
+++ src/components/FillBtn.jsx
@@ -1,15 +1,18 @@
-import React from 'react'
+import React from "react";
 
-const Fillbtn = ({hrefTo,text,className}) => {
+const Fillbtn = ({ onClick, text, className }) => {
   return (
-    // <a className={`inline-block mt-5  ${className}`} href="toChange">
-    <button className={`inline-block mt-5  ${className} relative border flex justify-center items-center border-brandGreen px-6 sm:px-16 h-10 sm:h-12 font-mono text-brandGreen text-xs sm:text-base tracking-widest
+    <button
+      onClick={onClick}
+      className={`inline-block mt-5  ${className} relative border flex justify-center items-center border-brandGreen px-6 sm:px-16 h-10 sm:h-12 font-mono text-brandGreen text-xs sm:text-base tracking-widest overflow-hidden
     after:absolute after:bottom-0 after:right-0
     after:w-2 after:h-2 after:bg-brandGreen
-    before:absolute before:-z-10 before:top-0 before:left-0 before:w-0 before:h-full before:bg-brandGreen
-    hover:text-black hover:before:w-full before:duration-300 `} >{text}</button>
-    // </a>
-  )
-}
+    before:absolute before:-z-10 before:top-0 before:-left-2 before:w-0 before:h-full before:bg-brandGreen before:-skew-x-12
+    hover:text-black hover:before:w-[110%] before:duration-300 `}
+    >
+      {text}
+    </button>
+  );
+};
 
-export default Fillbtn
+export default Fillbtn;
