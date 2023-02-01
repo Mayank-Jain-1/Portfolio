dc9242520fa175a45e8fde275a49df9bdb6425f5
Mayank <kaushalmayank.k@gmail.com>
2023-02-01 18:52:38 +0530

    added responsiveness for the map overlay div

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -27,7 +27,7 @@ const Map = () => {
         className="border-none lg:w-1/2 m-4 lg:ml-5 lg:mr-0 h-[300px] sm:h-[400px] md:h-[500px] lg:h-full
       overflow-hidden flex justify-center items-center relative"
       >
-        <div ref={ref} className={` absolute  duration-1000 opacity-0 bg-black bg-opacity-90 p-5 m-5 lg:m-12 top-0 left-0 text-white font-franklin text-xs md:text-base ${animate ? ' translate-y-0 opacity-100 animate-fadeIn' : 'translate-y-60'}`}>
+        <div ref={ref} className={` absolute  duration-1000 opacity-0 bg-black bg-opacity-90 p-3 sm:p-5 m-5 lg:m-12 top-0 left-0 text-white font-franklin text-[10px] sm:text-xs md:text-base ${animate ? ' translate-y-0 opacity-100 animate-fadeIn' : 'translate-y-60'}`}>
           <p>
             Mayank Jain, <br />
             Somewhere in <br />
