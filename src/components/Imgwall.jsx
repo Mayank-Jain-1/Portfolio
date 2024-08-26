74342fd04a4d44a0863779e591e12f67788afcb2
Mayank <mayankjainx404@gmail.com>
2024-08-26 10:44:29 +0530

    Added StreetFighter 2 project

--- src/components/Imgwall.jsx
+++ src/components/Imgwall.jsx
@@ -1,33 +1,61 @@
-import React from 'react'
-import Mjlogo from '../media/Mjlogo.png'
-import Netflix from '../media/Netflix.png'
-import PoseMaster from '../media/PoseMaster.png'
-import CloudCast from '../media/CloudCast.png'
-import NewsMonkey from '../media/NewsMonkey.png'
-import SpoonFed from '../media/SpoonFed.png'
-import DripX from '../media/DripX.png'
-import ImgwallImage from './ImgwallImage'
-import LinkedOut from '../media/LinkedOut.png'
-
+import React from 'react';
+import Mjlogo from '../media/Mjlogo.png';
+import Netflix from '../media/Netflix.png';
+import PoseMaster from '../media/PoseMaster.png';
+import CloudCast from '../media/CloudCast.png';
+import NewsMonkey from '../media/NewsMonkey.png';
+import SpoonFed from '../media/SpoonFed.png';
+import DripX from '../media/DripX.png';
+import ImgwallImage from './ImgwallImage';
+import LinkedOut from '../media/LinkedOut.png';
+import StreetFighter2 from '../media/StreetFighter2.png';
 
 const Imgwall = () => {
-  return (
-    <section>
+	return (
+		<section>
+			<div className="flex flex-wrap my-3">
+				<ImgwallImage
+					src={StreetFighter2}
+					projectLink="https://streetfighterbymayank.netlify.app/"
+				/>
+				<ImgwallImage
+					src={SpoonFed}
+					projectLink="https://spoon-fed.vercel.app/"
+				/>
 
+				<ImgwallImage
+					src={Netflix}
+					projectLink="https://mayank-jain-1.github.io/Netflix-Homepage.github.io/"
+				/>
+				<ImgwallImage
+					src={NewsMonkey}
+					projectLink="https://news-monkey-seven.vercel.app/"
+				/>
 
-    <div className='flex flex-wrap my-3'>
-      <ImgwallImage src={NewsMonkey} projectLink='https://news-monkey-seven.vercel.app/'/>
-      <ImgwallImage src={Netflix} projectLink="https://mayank-jain-1.github.io/Netflix-Homepage.github.io/"/>
-      <ImgwallImage src={CloudCast} projectLink='https://cloud-cast.vercel.app/'/>
-      <ImgwallImage src={PoseMaster} projectLink='https://github.com/Mayank-Jain-1/Pose-Master'/>
-      <ImgwallImage src={LinkedOut} projectLink='https://github.com/Mayank-Jain-1/LinkedOut'/>
-      <ImgwallImage src={Mjlogo}  projectLink='https://mayank-jain.vercel.app/'/>
-      <ImgwallImage src={SpoonFed} projectLink='https://spoon-fed.vercel.app/'/>
-      <ImgwallImage src={DripX} projectLink='https://mayank-jain-1.github.io/DripX.github.io/'/>
-    </div>
+				<ImgwallImage
+					src={PoseMaster}
+					projectLink="https://github.com/Mayank-Jain-1/Pose-Master"
+				/>
+				<ImgwallImage
+					src={LinkedOut}
+					projectLink="https://github.com/Mayank-Jain-1/LinkedOut"
+				/>
+				<ImgwallImage
+					src={Mjlogo}
+					projectLink="https://mayank-jain.vercel.app/"
+				/>
 
-    </section>
-  )
-}
+				<ImgwallImage
+					src={DripX}
+					projectLink="https://mayank-jain-1.github.io/DripX.github.io/"
+				/>
+				<ImgwallImage
+					src={CloudCast}
+					projectLink="https://cloud-cast.vercel.app/"
+				/>
+			</div>
+		</section>
+	);
+};
 
-export default Imgwall
+export default Imgwall;
--- src/components/ImgwallImage.jsx
+++ src/components/ImgwallImage.jsx
@@ -1,25 +1,38 @@
-import React from "react";
+import React from 'react';
 
-const ImgwallImage = ({className, src,projectLink}) => {
-
-  const defaultSrc = ''
-  return (
-    <div className={`${className} group
+const ImgwallImage = ({ className, src, projectLink }) => {
+	const defaultSrc = '';
+	return (
+		<div
+			className={`${className} group
     w-1/3 md:w-1/5
     flex items-center justify-center
-    relative overflow-hidden duration-300`}>
-      <img src={src || defaultSrc} alt="" className="w-full aspect-square brightness-75 md:brightness-50 group-hover:scale-110 group-hover:brightness-100 duration-300" />
-      <a href={projectLink} target='_blank'
-      rel="noreferrer" className="absolute top-1/2 left-1/2 
+    relative overflow-hidden duration-300`}
+		>
+			<img
+				src={src || defaultSrc}
+				alt=""
+				className="w-full aspect-square brightness-75 md:brightness-50 group-hover:scale-110 group-hover:brightness-100 duration-300"
+				height={500}
+				width={500}
+			/>
+			<a
+				href={projectLink}
+				target="_blank"
+				rel="noreferrer"
+				className="absolute top-1/2 left-1/2 
       -translate-x-1/2 -translate-y-1/2
       w-2/6 h-2/6 bg-brandGreen rounded-full border-none font-mono flex items-center justify-center text-center uppercase tracking-widest text-[6px] sm:text-[8px] md:text-[7px] lg:text-[10px]
       scale-50 duration-300 opacity-0
       group-hover:scale-100 group-hover:opacity-100
-      ">
-        View<br />Project
-      </a>
-    </div>
-  );
+      "
+			>
+				View
+				<br />
+				Project
+			</a>
+		</div>
+	);
 };
 
 export default ImgwallImage;
